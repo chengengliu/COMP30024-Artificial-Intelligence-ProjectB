03612a0e078c8f09d724fd034e3018c924b98a06
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-05-09 21:16:04 +1000

    Update

--- AIPlayer.py
+++ AIPlayer.py
@@ -1,5 +1,7 @@
 import time
-from AIStrategy import aiStrategy
+import PlayerFunction as pf
+import AIStrategy as Strategy
+
 class Player():
     def __int__(self, color):
         self.board = Board(color)
@@ -7,3 +9,17 @@ class Player():
         self.enemy = self.board.enemy
         self.player = self.board.player
         self.moving = False
+        self.turns = 0
+
+    def action(sefl,turns):
+        t = time.time()
+        if self.turns < 24:
+            action = self.strategy.placingPhase(self.board, self.player)
+        else:
+            action = self.startegy.move(self.board, self.player)
+        self.update(action,self.player)
+        return action
+
+    def update(self, action, color = None):
+        if self.turns < 24:
+            self.board.place(color,action)
