17315448a843dc1080f6c8c0de6e028bea46d759
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-05-11 10:18:34 +1000

    Add python doc and more comments

--- UtilityFunction.py
+++ UtilityFunction.py
@@ -5,6 +5,10 @@ EMPTY = "-"
 CENTER = 3.5
 
 class Utility:
+    '''
+    The Utility class is used for selecting the best actions in both moving
+    phase and placing phase.
+    '''
     def __init__(self,board):
         self.board = board
 
@@ -49,9 +53,21 @@ class Utility:
         return (piecesRemain(self.board),pieceDistance(self.board), fragilePiece(self.board))<=(piecesRemain(other.board),pieceDistance(other.board), fragilePiece(other.board))
 
 def piecesRemain(board):
+    '''
+    param: board
+    function: calculate the difference between player piece and opponent piece
+    return: the difference
+    '''
     return board.playerPieces - board.opponentPieces
 
 def pieceDistance(board):
+    '''
+    param: board
+    function: calculate average distance from center to each of a type of color.
+        (given a particular board state). The piece should move towards the center
+        and reduce the distance.
+    return: inverse of average distance.
+    '''
     distance = 0
     pieceNum = 0
     for column in range(len(board.grid)):
@@ -67,6 +83,13 @@ def pieceDistance(board):
         return -distance
 
 def fragilePiece(board):
+    '''
+    param: board
+    function: calculate total fragileness of a given type of piece(under a
+        particular board state). This fragileness indicates how easily one piece
+        can be eliminated. Yet basically that should be avoided.
+    return: inverse of fragileness
+    '''
     fragile = 0
     for column in range(len(board.grid)):
         for row in range(len(board.grid[column])):
